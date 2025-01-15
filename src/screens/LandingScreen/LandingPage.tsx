9e04d943d4d528ad8e818e04c4b472002f4cbeec
Manish Gond <manish.n.gond@gmail.com>
2025-01-15 19:45:39 +0530

    First Commit

--- src/screens/LandingScreen/LandingPage.tsx
+++ src/screens/LandingScreen/LandingPage.tsx
@@ -1,14 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const App = () => {
+type Props = {};
+
+const LandingPage = (props: Props) => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>LandingPage</Text>
     </View>
   );
 };
 
-export default App;
+export default LandingPage;
 
 const styles = StyleSheet.create({});
