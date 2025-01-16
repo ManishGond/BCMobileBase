927b3aae4688773557ef119d7bfcae9fc35e67de
Manish Gond <manish.n.gond@gmail.com>
2025-01-16 12:26:23 +0530

    Added Onboarding Swiper tool and needed to re-update the package.json for resolving the issue realted to "com.facebook.react.settings". which is now fixed in this update.

--- App.tsx
+++ App.tsx
@@ -2,18 +2,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import LandingPage from './src/screens/LandingScreen/LandingPage';
+import OnboardingScreenUno from './src/screens/LandingScreen/OnboardingScreenUno';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Onboarding">
       <Stack.Screen name="Landing" component={LandingPage} />
+      <Stack.Screen name="Onboarding" component={OnboardingScreenUno} />
     </Stack.Navigator>
   );
 }
 
-export default function App() {
+export default function App(): JSX.Element {
   return (
     <NavigationContainer>
       <RootStack />
---
+++ src/screens/LandingScreen/OnboardingScreenUno.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const OnboardingScreenUno = () => {
+  return <View style={styles.container}></View>;
+};
+
+export default OnboardingScreenUno;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+});
