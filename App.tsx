3de335cbece59621bf7eeee3a59c69c59add1cb3
Manish Gond <manish.n.gond@gmail.com>
2025-01-16 13:20:09 +0530

    Latest updated, with Lottie animation in Onboarding Screen

--- App.tsx
+++ App.tsx
@@ -2,15 +2,29 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import LandingPage from './src/screens/LandingScreen/LandingPage';
-import OnboardingScreenUno from './src/screens/LandingScreen/OnboardingScreenUno';
+import OnboardingScreen from './src/screens/LandingScreen/OnboardingScreen';
+
+export type RootStackParamList = {
+  Onboarding: undefined;
+  Landing: undefined;
+  Home: undefined;
+};
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
     <Stack.Navigator initialRouteName="Onboarding">
-      <Stack.Screen name="Landing" component={LandingPage} />
-      <Stack.Screen name="Onboarding" component={OnboardingScreenUno} />
+      <Stack.Screen
+        name="Landing"
+        options={{headerShown: false}}
+        component={LandingPage}
+      />
+      <Stack.Screen
+        name="Onboarding"
+        options={{headerShown: false}}
+        component={OnboardingScreen}
+      />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/LandingScreen/OnboardingScreen.tsx
@@ -0,0 +1,84 @@
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Onboarding from 'react-native-onboarding-swiper';
+import LottieView from 'lottie-react-native';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../App';
+
+const {width} = Dimensions.get('window');
+
+type OnboardingScreenNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+const OnboardingScreen = () => {
+  const navigation = useNavigation<OnboardingScreenNavigationProp>();
+  const handleDone = () => {
+    navigation.navigate('Landing');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Onboarding
+        onDone={handleDone}
+        onSkip={handleDone}
+        containerStyles={{paddingHorizontal: 15}}
+        pages={[
+          {
+            backgroundColor: '#a7f3d0',
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottieAnimation}
+                  source={require('../assets/lotte/animation1.json')}
+                  autoPlay={true}
+                  loop={true}
+                />
+              </View>
+            ),
+            subtitle: 'Done With react',
+            title: 'Welcome to BCMobile',
+          },
+          {
+            backgroundColor: '#fef3c7',
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottieAnimation}
+                  source={require('../assets/lotte/animation3.json')}
+                  autoPlay={true}
+                  loop={true}
+                />
+              </View>
+            ),
+            subtitle: 'Done With react',
+            title: 'Step 1',
+          },
+          {
+            backgroundColor: '#a78bfa',
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottieAnimation}
+                  source={require('../assets/lotte/animation2.json')}
+                  autoPlay={true}
+                  loop={true}
+                />
+              </View>
+            ),
+            subtitle: 'Done With react',
+            title: 'Step 2',
+          },
+        ]}
+      />
+    </View>
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+  lottieAnimation: {width: width * 0.9, height: width},
+});
